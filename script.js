dc6428e25ffbf14993888dd7ba34d5fdef8b8956
noelpaul <noelpaulnp075@gmail.com>
2026-05-26 13:35:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    const targetId = link.getAttribute('href').slice(1);
+    const target = document.getElementById(targetId);
+    if (target) {
+      event.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  });
+});
